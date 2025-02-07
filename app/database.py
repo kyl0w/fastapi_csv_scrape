b2693a37ffc262369ad22a70038e527eae4e444c
Tiago <lourenco123tiago@gmail.com>
2025-02-07 01:50:47 +0000

    FIX host db

--- app/database.py
+++ app/database.py
@@ -8,7 +8,7 @@ POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "rootuser")
 POSTGRES_DB = os.getenv("POSTGRES_DB", "csv_urls")
 POSTGRES_HOST = os.getenv("POSTGRES_HOST", "localhost")
 
-DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:5432/{POSTGRES_DB}"
+DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
 
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
